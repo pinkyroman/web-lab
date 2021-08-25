24a40818f526f4115dfb8b5f301f9423bd5176c0
김재동 <killem@uangel.com>
2021-08-25 17:49:17 +0900

    trainer removed.

--- backend/laravel/trainer/resources/views/my_welcome.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'My Welcome')
-
-@section('content')
-    <p >This is the my body content</p>
-    <example-component class="my-style"></example-component>
-    <my-component class="my-style"></my-component>
-@endsection

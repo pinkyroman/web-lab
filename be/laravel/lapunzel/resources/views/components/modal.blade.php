2d19f34a1df98ff4f22695c8d23656f9f4278384
김재동 <killemx@hotmail.com>
2021-09-05 12:58:23 +0900

    Blade <x-modal> component added

---
+++ be/laravel/lapunzel/resources/views/components/modal.blade.php
@@ -0,0 +1,21 @@
+<div class="modal fade" data-backdrop="static" data-keyboard="false" tabindex="-1" {{$attributes}}>
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">{{$title}}</h5>
+                <button type="button" class="close" data-dismiss="modal">
+                    <span>&times;</span>
+                </button>
+            </div>
+
+            <div class="modal-body">
+                {{$slot}}
+            </div>
+
+            <div class="modal-footer">
+                {{$buttons}}
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
--- be/laravel/lapunzel/resources/views/home.blade.php
+++ be/laravel/lapunzel/resources/views/home.blade.php
@@ -14,4 +14,34 @@
         ]
     ]
 ])
+
+<x-modal id="testModal" title="테스트">
+    <p>
+        모달 대화상자 테스트!
+    </p>
+
+    @slot('buttons')
+        <button type="button" class="btn btn-primary">Do Something</button>
+    @endslot
+</x-modal>
+
+<button type="button" class="btn btn-warning" data-toggle="modal" data-target="#testModal">Launch Modal</button>
+
+
+{{--
+@component('templates.modal')
+    @slot('id', 'testModal')
+    @slot('title', 'Modal Test')
+
+    <p>
+        모달 대화상자 테스트!
+    </p>
+
+    @slot('buttons')
+        <button type="button" class="btn btn-primary">Do Something</button>
+    @endslot
+@endcomponent --}}
+
 @endsection
+
+

9ea0316221cc178febf5fab4b875fb4eb0e04435
김재동 <killemx@hotmail.com>
2021-10-09 11:43:53 +0900

    clean up template codes for my own works

--- fe/Vue.js/html-css-training/src/App.vue
+++ fe/Vue.js/html-css-training/src/App.vue
@@ -1,32 +1,51 @@
 <template>
-  <div id="app">
-    <router-view/>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <div>
+            <img alt="Vue logo" src="./assets/logo.png" />
+            <h1>HTML/CSS/Vue.js Training</h1>
+        </div>
+        <div id="nav">
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link>
+        </div>
+        <router-view />
     </div>
-  </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        };
+    },
+    mounted() {
+        // console.log(this.$router.options.routes);
+    }
+}
+</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
+</style>
 
+<style scoped>
 #nav {
-  padding: 30px;
+    padding: 30px;
 }
 
 #nav a {
-  font-weight: bold;
-  color: #2c3e50;
+    font-weight: bold;
+    color: #2c3e50;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+    color: #42b983;
 }
 </style>
--- fe/Vue.js/html-css-training/src/views/Home.vue
+++ fe/Vue.js/html-css-training/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home"></div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import Contents from '@/components/Contents.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+    name: 'Home',
+    components: {
+        //Contents
+    }
 }
 </script>
+
+<style scoped>
+</style>

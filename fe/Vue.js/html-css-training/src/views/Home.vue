28a6fefdd7796eb736d09ad2c2cc3c70e723addb
김재동 <killem@uangel.com>
2021-10-13 12:12:29 +0900

    test code added.

--- fe/Vue.js/html-css-training/src/views/Home.vue
+++ fe/Vue.js/html-css-training/src/views/Home.vue
@@ -1,5 +1,47 @@
 <template>
-    <div class="home"></div>
+    <div class="home">
+        <table>
+            <colgroup>
+                <col/>
+                <col/>
+                <col/>
+                <col/>
+                <col/>
+            </colgroup>
+
+            <thead>
+                <tr>
+                    <th colsapn="1">COLUMN_HEADER1</th>
+                    <th colsapn="1">CONLUMN_HEADER2</th>
+                </tr>
+                <tr>
+                    <th>inner_key</th>
+                    <th>chechbox</th>
+                    <th>첫 번째</th>
+                    <th>두 번째</th>
+                    <th>세 번째</th>
+                </tr>
+            </thead>
+            
+            <tbody>
+                <!-- Row definitions comes here -->
+            </tbody>
+
+            <tfoot>
+                <tr>
+                    <th colsapn="1">COLUMN_HEADER1</th>
+                    <th colsapn="1">CONLUMN_HEADER2</th>
+                </tr>
+                <tr>
+                    <th style="display:none">inner_key</th>
+                    <th style="display:none">checkbox</th>
+                    <th>첫 번째 요약</th>
+                    <th>두 번째 요약</th>
+                    <th>세 번째 요약</th>
+                </tr>
+            </tfoot>
+        </table>
+    </div>
 </template>
 
 <script>
@@ -15,4 +57,9 @@ export default {
 </script>
 
 <style scoped>
+table {
+    border: 1px solid black;
+    border-radius: 5px;
+    padding: 0px;
+}
 </style>

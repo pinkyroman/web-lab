8b3ed4be64709a8883e5318edb43b38b0bc61371
김재동 <killem@uangel.com>
2021-10-29 15:23:20 +0900

    vue router 추가

--- fe/Vuetify/basics/src/App.vue
+++ fe/Vuetify/basics/src/App.vue
@@ -1,15 +1,35 @@
 <template>
-    <v-app>Hello, World!</v-app>
+  <v-app>
+    <v-main>
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
+
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-<script>
-export default {
-    data() {
-        return {};
-    }
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-</script>
 
-<style scoped>
+#nav {
+  padding: 30px;
 
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
 </style>
---
+++ fe/Vuetify/basics/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <div>Hello, World!</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld
+  },
+};
+</script>

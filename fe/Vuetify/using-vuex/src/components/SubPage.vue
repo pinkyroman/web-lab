11d09cd2b40052221da996691e714dbe9deb3c5f
김재동 <killemx@hotmail.com>
2021-11-20 22:52:09 +0900

    updated

---
+++ fe/Vuetify/using-vuex/src/components/SubPage.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-container>
+        <v-main>
+            <div class="text-center display-1 my-4">서브 페이지 입니다.</div>
+            <v-divider></v-divider>
+            <div class="text-center display-3 my-4">{{ title }}</div>
+            <div class="text-center">
+                <v-btn fab large class="mt-5" color="teal" dark to="/main">
+                    <v-icon>mdi-replay</v-icon>
+                </v-btn>
+            </div>
+        </v-main>
+    </v-container>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: this.$store.getters.getData,
+        };
+    },
+};
+</script>
--- fe/Vuetify/using-vuex/src/store/index.js
+++ fe/Vuetify/using-vuex/src/store/index.js
@@ -4,12 +4,23 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  // title 값을 상탯값으로 정의
   state: {
+    title: '',
   },
+  // mutations는 getters와 대칭되는 setters 역할을 한다.
   mutations: {
+    setData(state, payload) {
+      return state.title = payload;
+    },
   },
-  actions: {
+  // actions는 mutations와 대칭되는 getters 역할을 한다.
+  getters: {
+    getData(state) {
+      return state.title;
+    },
   },
   modules: {
   }
-})
+});
+

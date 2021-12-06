ba7a855d67ecf42c05fd7e9d5425f47c1e55a640
김재동 <killem@uangel.com>
2021-12-06 16:56:01 +0900

    js sample codes

--- pl/js/basics/es6-module-main.mjs
+++ pl/js/basics/es6-module-main.mjs
@@ -4,3 +4,13 @@ let p1 = Person('alice', 'cooper', 88);
 let p2 = new Person('ozzy', 'osbourne', 90);
 console.log(p2.getFirstName());
 console.log(p1.getFirstName());
+
+console.log('"abc": ' + typeof "abc");
+console.log('123: ' + typeof 123);
+console.log('false: ' + typeof false);
+console.log('Symbol("x"): ' + typeof Symbol('x'));
+console.log('{}: ' + typeof {});
+console.log('[]: ' + typeof []);
+console.log('Math: ' + typeof Math);
+console.log('Array: ' + typeof Array);
+console.log('(function () { }): ' + typeof (function () { }));

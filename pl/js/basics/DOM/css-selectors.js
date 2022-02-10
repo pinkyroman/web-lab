ec9fc8e507939e780de2f4ba197466603523184a
김재동 <killemx@hotmail.com>
2022-02-10 20:13:25 +0900

    DOM example modified

--- pl/js/basics/DOM/css-selectors.js
+++ pl/js/basics/DOM/css-selectors.js
@@ -1,6 +1,14 @@
 // colors
 const colors = {
-    'box-red': {
+    'theme-dark': {
+        'color': '#212121',
+        'lighten1': '#424242',
+        'lighten2': '#616161',
+        'lighten3': '#757575',
+        'lighten4': '#BDBDBD',
+        'lighten5': '#E0E0E0',
+    },
+    'theme-red': {
         'color': '#F44336',
         'lighten1': '#EF5350',
         'lighten2': '#E57373',
@@ -8,7 +16,7 @@ const colors = {
         'lighten4': '#FFCDD2',
         'lighten5': '#FFEBEE',
     },
-    'box-purple': {
+    'theme-purple': {
         'color': '#9C27B0',
         'lighten1': '#AB47BC',
         'lighten2': '#BA68C8',
@@ -17,61 +25,6 @@ const colors = {
         'lighten5': '#F3E5F5',
     },
 };
-
-// style box class
-const boxes = document.querySelectorAll('.box');
-boxes.forEach(box => {
-    const style = box.style;
-    style.margin = '8px 4px';
-    style.padding = '4px';
-    style.border = '1px solid';
-    style.borderRadius = '0px 10px';
-});
-
-const boxTitles = document.querySelectorAll('.box-title');
-boxTitles.forEach(boxTitle => {
-    const style = boxTitle.style;
-    style.margin = '2px';
-    style.padding = '4px';
-    style.border = '1px solid';
-    style.borderRadius = '0px 10px 0px 0px';
-
-    style.fontSize = '1.2em';
-    style.fontWeight = 'bold';
-});
-
-const boxContents = document.querySelectorAll('.box-content');
-boxContents.forEach(boxContent => {
-    const style = boxContent.style;
-    style.margin = '4px 2px';
-    style.padding = '8px';
-    style.border = '1px solid';
-    style.borderRadius = '0px 0px 0px 10px';
-});
-
-document.querySelectorAll('.box.box-red').forEach(box => {
-    setBoxColor(box, 'box-red');
-});
-
-document.querySelectorAll('.box-red > .box-title').forEach(boxTitle => {
-    setBoxTitleColor(boxTitle, 'box-red');
-});
-
-document.querySelectorAll('.box-red > .box-content').forEach(boxContent => {
-    setBoxContentColor(boxContent, 'box-red');
-});
-
-document.querySelectorAll('.box.box-purple').forEach(box => {
-    setBoxColor(box, 'box-purple');
-});
-
-document.querySelectorAll('.box-purple > .box-title').forEach(boxTitle => {
-    setBoxTitleColor(boxTitle, 'box-purple');
-});
-
-document.querySelectorAll('.box-purple > .box-content').forEach(boxContent => {
-    setBoxContentColor(boxContent, 'box-purple');
-});
 
 function setBoxColor(box, theme) {
     const c = colors[theme];
@@ -98,3 +51,65 @@ function setBoxContentColor(boxContent, theme) {
     style.backgroundColor = c.lighten2;
     style.borderColor = c.lighten2;
 }
+
+// style box class
+const boxes = document.querySelectorAll('.box');
+boxes.forEach(box => {
+    const style = box.style;
+    style.marginTop = '8px';
+    style.padding = '4px';
+    style.border = '1px solid';
+    style.borderRadius = '0px 10px';
+});
+
+const boxTitles = document.querySelectorAll('.box-title');
+boxTitles.forEach(boxTitle => {
+    const style = boxTitle.style;
+    style.margin = '2px';
+    style.padding = '4px';
+    style.border = '1px solid';
+    style.borderRadius = '0px 10px 0px 0px';
+
+    style.fontSize = '1.2em';
+    style.fontWeight = 'bold';
+});
+
+const boxContents = document.querySelectorAll('.box-content');
+boxContents.forEach(boxContent => {
+    const style = boxContent.style;
+    style.margin = '4px 2px';
+    style.padding = '8px';
+    style.border = '1px solid';
+    style.borderRadius = '0px 0px 0px 10px';
+});
+
+function appyTheme(theme) {
+    document.querySelectorAll(`.box.${theme}`).forEach(box => {
+        setBoxColor(box, theme);
+    });
+    
+    document.querySelectorAll(`.${theme} > .box-title`).forEach(boxTitle => {
+        setBoxTitleColor(boxTitle, theme);
+    });
+    
+    document.querySelectorAll(`.${theme} > .box-content`).forEach(boxContent => {
+        setBoxContentColor(boxContent, theme);
+    });
+}
+
+document.getElementsByName('opt-box-theme').forEach(radio => {
+    radio.addEventListener('click', function() {        
+        if (this.checked) {
+            const theme = this.value;
+            document.querySelectorAll('.box').forEach(box => {
+                box.classList.forEach(className => {
+                    if (className.startsWith('theme-')) {
+                        box.classList.remove(className);
+                    }
+                });
+                box.classList.add(theme);                
+            });
+            appyTheme(theme);
+        }
+    });
+});

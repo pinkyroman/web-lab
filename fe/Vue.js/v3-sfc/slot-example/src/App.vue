268a283ce7555387a39b467801d472e619837e86
김재동 <killem@uangel.com>
2022-03-24 08:25:03 +0900

    slot example

---
+++ fe/Vue.js/v3-sfc/slot-example/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <AppCard>
+    <template #title>
+      HERE COMES THE DARKNESS
+    </template>
+
+    <div>
+      THIS IS MY OWN CONTENT.
+    </div>
+
+    <template #footer="{ versionInfo }">
+      This is just built for testing purpose (version: {{ versionInfo.version }}, release: {{ versionInfo.releaseDate }}).
+    </template>
+  </AppCard>
+</template>
+
+<style>
+
+</style>
+
+<script setup>
+import AppCard from './components/AppCard.vue';
+
+</script>
+

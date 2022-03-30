33c95b6aaa6f9e90c091bb55e31acaf21b71548a
김재동 <killem@uangel.com>
2022-03-30 11:15:29 +0900

    pwa examples removed

--- fe/Vue.js/v3-sfc/router-and-pinia/src/stores/counter.js
+++ fe/Vue.js/v3-sfc/router-and-pinia/src/stores/counter.js
@@ -6,11 +6,15 @@ export const useCounterStore = defineStore({
     counter: 0,
   }),
   getters: {
+    value: (state) => state.counter,
     doubleCount: (state) => state.counter * 2,
   },
   actions: {
     increment() {
       this.counter++;
+    },
+    decrement() {
+      this.counter--;
     },
   },
 });

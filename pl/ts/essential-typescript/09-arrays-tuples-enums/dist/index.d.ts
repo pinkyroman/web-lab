674dc25a349f5466ade3fa823fc92284828b11c4
김재동 <killem@uangel.com>
2022-05-24 16:03:47 +0900

    studying ts

---
+++ pl/ts/essential-typescript/09-arrays-tuples-enums/dist/index.d.ts
@@ -0,0 +1,5 @@
+declare function calculateTax(amount: number): number;
+declare function writePrice(product: string, price: number): void;
+declare let hatPrice: number;
+declare let glovesPrice: number;
+declare let umbrellaPrice: number;
